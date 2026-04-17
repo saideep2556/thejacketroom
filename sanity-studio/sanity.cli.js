5bcd17531807b41617de91b9b48376c422edd8ba
Saideep <saideepsomineni99@gmail.com>
2026-04-17 12:20:31 +0530

    adding sanity.cls

---
+++ sanity-studio/sanity.cli.js
@@ -0,0 +1,8 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'l6jpnjpm',  // ← your actual project ID here
+    dataset: 'production'
+  }
+})
